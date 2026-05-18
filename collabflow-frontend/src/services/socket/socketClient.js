35b02c46433c45889499cd9c9f6e03a67c739711
Dhrubashis Basak <dhrubashis.basak@gmail.com>
2026-05-18 14:20:31 +0530

    Production Details updated

--- collabflow-frontend/src/services/socket/socketClient.js
+++ collabflow-frontend/src/services/socket/socketClient.js
@@ -6,7 +6,7 @@ class SocketService {
     connect(token) {
         if (this.socket?.connected) return;
 
-        this.socket = io("http://localhost:5000", {
+        this.socket = io("https://collabflow-luhs.onrender.com", {
             auth: {
                 token,
             },
